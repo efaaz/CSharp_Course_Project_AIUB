01e5e90a4554353b6b9aa46edf6599e79acd081b
Wasifur Rahman Efaz <rahmanefazwasifur@gmail.com>
2026-09-14 01:04:16 +0600

    db changes

--- Data/DBConnection.cs
+++ Data/DBConnection.cs
@@ -6,13 +6,11 @@ namespace Restaurant_Management.Data
     public static class DBConnection
     {
         // Sohag's String
-        private static readonly string ConnectionString = "Data Source=DESKTOP-U1CVD57;Initial Catalog=Restaurent Management;Integrated Security=True;";
+        // private static readonly string ConnectionString = "Data Source=DESKTOP-U1CVD57;Initial Catalog=Restaurent Management;Integrated Security=True;";
 
         // Efaz's String
  
-         //private static readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Restaurant Managemnet;Integrated Security=True;";
-
-        //private static readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Restaurant Managemnet;Integrated Security=True;";
+         private static readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Restaurant Managemnet;Integrated Security=True;";
 
 
         // Fatema's String
